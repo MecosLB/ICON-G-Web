d10ab405c720ccf0f04f0928718e2224d442edbf
Fernando A Jacobo <fernandojacobo54@gmail.com>
2024-05-14 08:59:23 -0600

    Add divider

---
+++ src/components/Divider.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="divider-icong">
+        <span class="bg-yellow"></span>
+        <span class="bg-purple"></span>
+        <span class="bg-green-aqua"></span>
+        <span class="bg-pink"></span>
+    </div>
+</template>
