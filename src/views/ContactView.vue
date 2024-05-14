b87618630e0cbfcae1f80881ea6015e6210e9ed6
Fernando A Jacobo <fernandojacobo54@gmail.com>
2024-05-14 11:56:44 -0600

    Contact & Support Page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,3 +1,83 @@
 <template>
-    <section id="contact"></section>
+    <section id="contact">
+        <div class="hero-image mb-5">
+            <div class="hero-text">
+                <h4 class="mb-5"> Contacto </h4>
+                <Divider />
+            </div>
+        </div>
+
+        <div class="container mt-5">
+            <div class="row title-view">
+                <h6> Ubicación </h6>
+            </div>
+        </div>
+
+        <div class="container-fluid p-0 mt-5">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3733.1218501703283!2d-103.39057522578845!3d20.664621100187873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428ae76779a47c3%3A0x5326f5c14a77a264!2sCalle%20de%20la%20Nebulosa%202913%2C%20Jardines%20del%20Bosque%2C%2044520%20Guadalajara%2C%20Jal.!5e0!3m2!1ses-419!2smx!4v1715700703754!5m2!1ses-419!2smx" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+        </div>
+
+        <div class="container mt-5">
+            <div class="row title-view">
+                <h6> Contáctenos </h6>
+            </div>
+        </div>
+
+        <div class="container-fluid mt-5 mb-5 p-0 container-contact">
+            <div class="container d-flex justify-content-center flex-column">
+                <div class="d-flex flex-wrap align-items-center justify-content-center mb-5">
+                    <div class="col-12 col-sm-12 col-md-5 p-5 ">
+                        <div class="row mb-4">
+                            <h6 class="text-uppercase"> Dirección </h6>
+                            <p>
+                                Calle de la Nebulosa 2913, Jardines
+                                del Bosque, 44520 Guadalajara, Jal.
+                            </p>
+                        </div>
+
+                        <div class="row">
+                            <h6 class="text-uppercase"> Contacto </h6>
+                            <p> 01 (33) 3333 3333 </p>
+                            <p> contacto@icong.com </p>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-sm-12 col-md-5 p-5 ">
+                        <form action="POST" autocomplete="off">
+                            <div class="row mb-3">
+                                <input type="text" class="form-control form-control-sm rounded-0" placeholder="Nombre">
+                            </div>
+
+                            <div class="row mb-3">
+                                <input type="tel" class="form-control form-control-sm rounded-0" placeholder="Teléfono">
+                            </div>
+
+                            <div class="row mb-3">
+                                <textarea class="form-control form-control-sm rounded-0" id="" rows="1" placeholder="Mensaje"></textarea>
+                            </div>
+
+                            <div class="row mb-3">
+                                <button type="button" class="btn btn-lg btn-primary btn-contact">
+                                    Enviar Mensaje
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="d-flex align-items-center justify-content-center">
+                    <Divider />
+                </div>
+            </div>
+        </div>
+
+        <div class="container mt-5 p-5"></div>
+    </section>
 </template>
+
+<script setup>
+import Divider from '@/components/Divider.vue';
+</script>
--- src/views/SupportView.vue
+++ src/views/SupportView.vue
@@ -1,3 +1,99 @@
 <template>
-    <section id="support"></section>
+    <section id="support">
+        <div class="hero-image mb-5">
+            <div class="hero-text">
+                <h4 class="mb-5"> Soporte </h4>
+                <Divider />
+            </div>
+        </div>
+
+        <div class="container mt-5 mb-5">
+            <div class="row title-view">
+                <h6> Pregunta Frecuentes </h6>
+            </div>
+        </div>
+
+        <div class="container-fluid p-0 bg-gray">
+            <div class="container">
+                <div class="p-5">
+                    <div class="card bg-gray">
+                        <div class="card-body bg-gray">
+                            <div class="accordion accordion-flush" id="accordion">
+                                <div v-for="({ id, user, date, question, response }, index) in questions"
+                                    class="accordion-item">
+                                    <h2 class="accordion-header">
+                                        <button class="accordion-button collapsed" type="button"
+                                            data-bs-toggle="collapse" :data-bs-target="`#flush-collapse${id}`"
+                                            aria-expanded="false" :aria-controls="`flush-collapse${id}`">
+                                            <div class="details-accordion">
+                                                <div class="d-flex flex-wrap mb-3">
+                                                    <div class="col-12 col-sm-12 mb-3 mb-sm-0 col-md-6">
+                                                        <strong> Usuario: {{ user }} </strong>
+                                                    </div>
+
+                                                    <div class="col-12 col-sm-12 mb-3 mb-sm-0 col-md-6 text-end">
+                                                        <span v-html="date"></span>
+                                                    </div>
+                                                </div>
+
+                                                <div class="d-flex flex-wrap">
+                                                    <div class="col-12 col-sm-12 mb-3 mb-sm-0 col-md-6">
+                                                        <span v-html="question"></span>
+                                                    </div>
+
+                                                    <div class="col-12 col-sm-12 mb-3 mb-sm-0 col-md-6 text-end">
+                                                        <span class="see-details">
+                                                            Ver detalles
+                                                            <i class="fa-solid fa-arrow-right-long ms-2"></i>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </button>
+                                    </h2>
+
+                                    <div :id="`flush-collapse${id}`" class="accordion-collapse collapse"
+                                        data-bs-parent="#accordion">
+                                        <div class="accordion-body" v-html="response"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container mt-5 p-5"></div>
+    </section>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+import Divider from '@/components/Divider.vue';
+
+const questions = ref([
+    {
+        id: '1',
+        user: 'Administrador',
+        date: '14 de Mayo 2024',
+        question: '¿Como entrar al sistema ICON G?',
+        response: 'Placeholder content for this accordion, which is intended to demonstrate the.'
+    },
+    {
+        id: '2',
+        user: 'Administrador',
+        date: '10 de Mayo 2024',
+        question: 'Olvidé mi contraseña ¿cómo la recupero?',
+        response: 'Placeholder content for this accordion, which is intended to demonstrate the.'
+    },
+    {
+        id: '3',
+        user: 'Administrador',
+        date: '01 de Mayo 2024',
+        question: '¿Cómo generar un reporte general?',
+        response: 'Placeholder content for this accordion, which is intended to demonstrate the.'
+    },
+]);
+</script>
